905ac1787b4a7334d93d7adb47e55a36b835c28c
wyyden <ankudinoff.daniil@yandex.ru>
2022-02-24 18:03:44 +0300

    add function to choose symbol and add function to show field

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+from random import randint
+
+
+def get_user_symbol():
+    while True:
+        user_symbol = input('Enter symbol for game: ').strip(' ').upper()
+        if user_symbol not in ('X', 'O'):
+            print('Symbol is\'t X or O')
+            continue
+        else:
+            return user_symbol
+
+
+def show_field():
+    for x in range(3):
+        for y in range(3):
+            print('|', end='')
+            print(x + y + 1, end='')
+        print('|')
+
 
 if __name__ == '__main__':
-    pass
+    get_user_symbol()
+    show_field()
